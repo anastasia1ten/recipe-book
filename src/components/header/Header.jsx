b6a60d27f4dccb61a58a6e7195f602fb7a35246f
anastasia1ten <neymananastasia73@gmail.com>
2026-05-28 21:35:45 +0300

    добавила базовые компоненты

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+export const Header = () => {
+    return (
+        <header>
+            <div>
+                // ТУТ ЛОГОТИП ИЛИ ТЕКСТ
+            </div>
+            <nav>
+                // ТУТ НАВИГАЦИЯ
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
